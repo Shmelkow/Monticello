b6d51200c885221accfe4fc3becf3a98f18939ff
Shmelkow <sol2390@gmail.com>
2020-01-06 20:43:37 +0200

    img fix

--- javascript/app.js
+++ javascript/app.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
+        //autoplay: true,
         autoplaySpeed: 4000,
     });
 
